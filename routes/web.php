921a3f85e87c32542e974cd82ccb41b8da3429d4
kenberu-dev <kenberu.development@gmail.com>
2025-02-25 16:16:08 +0900

    ルーティングの実装

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 Route::middleware('auth' , 'verified')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::post('/send', [DashboardController::class, 'send'])->name('send');
+    Route::post('/dashboard', [DashboardController::class, 'send'])->name('send');
 });
 
 Route::middleware('auth')->group(function () {
