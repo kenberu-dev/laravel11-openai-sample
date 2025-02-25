749d8725ec51ba30d9ffac539e6a5b17dd8c4609
kenberu-dev <kenberu.development@gmail.com>
2025-02-25 16:15:54 +0900

    チャットページの作成

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,22 +1,48 @@
+import InputLabel from '@/Components/InputLabel';
+import TextAreaInput from '@/Components/TextAreaInput';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head, useForm } from '@inertiajs/react';
+import ReactMarkdown from 'react-markdown';
 
-export default function Dashboard() {
+export default function Dashboard({ message }) {
+    const { data, setData, post, errors } = useForm({
+        prompt: '',
+    })
+
+    const  onSubmit = (e) => {
+        e.preventDefault();
+        post(route('send'));
+    }
     return (
         <AuthenticatedLayout
             header={
                 <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Dashboard
+                    ダッシュボード
                 </h2>
             }
         >
-            <Head title="Dashboard" />
+            <Head title="ダッシュボード" />
 
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            You're logged in!
+                            <form onSubmit={onSubmit}>
+                                <InputLabel value="質問" />
+                                <TextAreaInput
+                                    className="mt-1 block w-full"
+                                    onChange={(e) => setData('prompt', e.target.value)}
+                                />
+                                <button
+                                    className='btn btn-primary mt-4'
+                                >
+                                    送信
+                                </button>
+                            </form>
+                        </div>
+                        <div className="p-6 text-gray-900">
+                            <InputLabel value="回答" />
+                            <ReactMarkdown>{message}</ReactMarkdown>
                         </div>
                     </div>
                 </div>
